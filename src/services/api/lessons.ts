0452fb01f32486919659dc10b22722ada5fcfd55
Thiago Corta <thiago.corta@gmail.com>
2024-10-10 16:00:42 +0200

    Add cors proxy

--- src/services/api/lessons.ts
+++ src/services/api/lessons.ts
@@ -1,6 +1,6 @@
 export async function getLessons() {
   const response = await fetch(
-    'https://fake-door-develop.azurewebsites.net/api/lessons/get-simple'
+    'https://corsproxy.io/?https%3A%2F%2Ffake-door-develop.azurewebsites.net%2Fapi%2Flessons%2Fget-simple'
   );
 
   return response?.json();
